254dacaafbac28e4373697439b18abab873b4019
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-09-20 03:07:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def post_uid_finder():
             try:
                 resp = requests.get(fb_url)
                 text = resp.text
-                patterns = [r"/posts/(\d+)r"story_fbid=(\d+)"r"""facebook\.com.*?/photos/\d+/(\d+)"]
+                patterns = [ r"/posts/(\\d+)", r"story_fbid=(\\d+)", r"facebook\\.com.*?/photos/\\d+/(\\d+)"]
                 for pat in patterns:
                     match = re.search(pat, text)
                     if match:
