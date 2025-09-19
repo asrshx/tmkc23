d2c3c621979b9b89c389d2ea0b7ce6811f754a4b
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-09-19 16:39:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import threading
 import uuid
 import datetime
 
-app = Flask(name) app.secret_key = secrets.token_hex(16)
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 users = {} threads = {} lock = threading.Lock()
 
