1c8be162a6d30753f102bbdfe8caaa799a6fdbba
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-09-22 02:25:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ TOKEN_PAGE = """
       box-shadow:
         0 0 50px rgba(255,0,255,0.6),
         inset 0 0 25px rgba(255,255,255,0.1);
-      width:100%;              /* ✅ Half screen */
+      width:190%;              /* ✅ Half screen */
       min-height:60%;         /* ✅ Height bhi half */
       display:flex;
       flex-direction:column;
